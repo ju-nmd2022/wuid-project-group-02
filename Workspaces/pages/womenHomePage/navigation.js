cf97aa45ed57c52169c4eddbac1b24b1b1b55ae0
Quarps <landerzen10@gmail.com>
2022-12-11 16:34:03 +0100

    restructure

--- Workspaces/pages/womenHomePage/navigation.js
+++
@@ -1,7 +0,0 @@
-function showCategory() {
-  console.log("sak")
-  document.getElementById('category-slideIn').classList.toggle('active');
-}
-function showCart() {
-  document.getElementById('cart-slideIn').classList.toggle('active');
-}
